a348c0104b03622e22250fe2060eac8480b5fb91
LAgrl <lizethariasc14@gmail.com>
2021-04-26 22:09:21 -0700

    function fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,24 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's game?");
+// Note the lack of quotation mark around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a mesage");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+window.alert("The fight has begun!");
+}
+
+
+
+
+
+
+
+
+
+fight();
